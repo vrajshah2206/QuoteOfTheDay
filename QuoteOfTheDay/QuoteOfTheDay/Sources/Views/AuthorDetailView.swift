f4940ca4be952e2cf5544ddab0cc2503eefdccf2
Savan <savansavani111@gmail.com>
2023-06-22 21:43:15 -0400

    json data refactores

--- QuoteOfTheDay/QuoteOfTheDay/Sources/Views/AuthorDetailView.swift
+++ QuoteOfTheDay/QuoteOfTheDay/Sources/Views/AuthorDetailView.swift
@@ -2,11 +2,7 @@
 //  AuthorDetailView.swift
 //  QuoteOfTheDay
 //
-<<<<<<< Updated upstream
 //  Created by Savan Savani on 2023-06-22.
-=======
-//  Created by Mehul on 2023-06-22.
->>>>>>> Stashed changes
 //
 
 import SwiftUI
@@ -29,13 +25,7 @@ struct AuthorDetailView: View {
                 .font(.body)
                 .padding()
             
-<<<<<<< Updated upstream
-=======
-            Text("Detailed Data:\n\(detailedData)")
-                .font(.body)
-                .padding()
-            
->>>>>>> Stashed changes
+
             Text("Expertise: \(expertise)")
                 .font(.body)
                 .padding()
@@ -47,14 +37,10 @@ struct AuthorDetailView: View {
             Text("Achievements: \(achievements)")
                 .font(.body)
                 .padding()
-            
-<<<<<<< Updated upstream
-            Text("Detailed Data:\n\(detailedData)")
+                        Text("Detailed Data:\n\(detailedData)")
                 .font(.body)
                 .padding()
             
-=======
->>>>>>> Stashed changes
             Spacer()
         }
         .navigationTitle("Author Details")
