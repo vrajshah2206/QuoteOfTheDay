6bfac35e7dad6240f55c7a5bd4be0562b4c4c92a
Vikas <vikaspanwar971@gmail.com>
2023-07-30 18:46:50 -0400

    colour fixes
    Text colour changed according to the background colour

--- QuoteOfTheDay/QuoteOfTheDay/Sources/Views/RandomQuoteView.swift
+++ QuoteOfTheDay/QuoteOfTheDay/Sources/Views/RandomQuoteView.swift
@@ -11,14 +11,14 @@ struct RandomQuoteView: View {
     @Environment(\.colorScheme) var colorScheme
     @State private var size = 0.8
     @State private var opacity = 0.5
-
+    
     @State private var randomQuote: Quote?
-
+    
     let quotes: [Quote] = {
         guard let fileURL = Bundle.main.url(forResource: "quotes", withExtension: "json") else {
             fatalError("Failed to locate quotes.json file")
         }
-
+        
         do {
             let data = try Data(contentsOf: fileURL)
             let decoder = JSONDecoder()
@@ -28,54 +28,54 @@ struct RandomQuoteView: View {
             fatalError("Failed to decode quotes.json: \(error)")
         }
     }()
-
+    
     func generateRandomQuote() {
         randomQuote = quotes.randomElement()
     }
-
+    
     var body: some View {
         NavigationView {
-                    VStack {
-                        VStack {
-                            if let quote = randomQuote {
-                                Text(quote.quote)
-                                    .font(.title3)
-                                    .frame(width: 350, height: 230)
-                                    .multilineTextAlignment(.center)
-                                    .padding(.horizontal)
-                                    .foregroundColor(.white)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 20)
-                                            .fill(LinearGradient(
-                                                gradient: Gradient(colors: [colorScheme == .light ? .purple : .gray, colorScheme == .light ? .blue : .white]),
-                                                startPoint: .bottomLeading,
-                                                endPoint: .bottomTrailing
-                                            ))
-                                            .frame(width: 350, height: 230))
-                                    .cornerRadius(10)
-                                
-                                
-                                Text("\(quote.author)")
-                                    .font(.title2)
-                                    .foregroundColor(.black)
-                            } else {
-                                Text("No quotes available")
-                                    .foregroundColor(.red)
-                            }
-                        }
-                       
+            VStack {
+                VStack {
+                    if let quote = randomQuote {
+                        Text(quote.quote)
+                            .font(.title3)
+                            .foregroundColor(colorScheme == .light ? .white : .black)
+                            .frame(width: 350, height: 230)
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(LinearGradient(
+                                        gradient: Gradient(colors: [colorScheme == .light ? .purple : .gray, colorScheme == .light ? .blue : .white]),
+                                        startPoint: .bottomLeading,
+                                        endPoint: .topTrailing
+                                    ))
+                                    .frame(width: 350, height: 230))
+                            .cornerRadius(10)
+                        
+                        Text("\(quote.author)")
+                            .font(.title2)
+                            .foregroundColor(.black)
+                    } else {
+                        Text("No quotes available")
+                            .foregroundColor(.red)
                     }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .padding(3)
-                    .background( colorScheme == .light ?
-                                 Image("11")
-                        .resizable()
-                        .edgesIgnoringSafeArea(.all) :  Image("14")
-                        .resizable()
-                        .edgesIgnoringSafeArea(.all)
-                    )
-                    
                 }
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(3)
+            .background( colorScheme == .light ?
+                         Image("11")
+                .resizable()
+                .edgesIgnoringSafeArea(.all) :  Image("14")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            )
+            
+        }
         .onAppear(perform: generateRandomQuote)
     }
 }
